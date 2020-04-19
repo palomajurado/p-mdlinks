c942ca0fd462fa86d4bdafb6d249f13b38abb021
Paloma Jurado <51180179+sambasilio@users.noreply.github.com>
2020-04-19 16:58:33 -0500

    TP-I1/Config. build of Babel & lint of it
    * Config jest.
    * Config eslint.
    * Modify scripts at    package.json.

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,3 +1,6 @@
+/* eslint-disable indent */
+/* eslint-disable eol-last */
+/* eslint-disable no-console */
 describe('mdLinks', () => {
     it('should...', () => console.log('WIP')); // Work in progress
-}); //creando test basico para preparar entorno de los test futuros
+}); // creando test basico para preparar entorno de los test futuros
